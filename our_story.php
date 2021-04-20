e9d15833709943f65ac937c1bbdf388206155fb2
G-Apxo <giorgi.a@synvestment.co>
2021-04-20 17:52:26 +0400

    final update

--- our_story.php
+++ our_story.php
@@ -43,13 +43,227 @@
                                  In 2013, a couple of maverick Georgian winemakers such as Baia Abuladze and her sister Gvantsa started to rediscover
                                  the rarest species of wine grapes in Western Georgia and to focus on bio wine making techniques. The re-discovery process
                                  continues until today: Baia Abuladze, together with her winemaking family, stands among those Georgian winemakers
-                                 who work to identify and classify local vine varieties
+                                 who work to identify and classify local vine varieties.
                               </div>
                            </div>
                         </div>
                      </div>
                   </div>
-                  <div class="sc_columns_item"><img alt="" src="images/bottles/tsitska-tsolakauri.png"></div>
+                  <div class="sc_columns_item"><img alt="" src="images/baias-qvevry.png"></div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="sc_content mainWrap">
+         <div class="sc_event">
+            <div class="sc_event_item">
+               <div class="sc_columns_2 sc_columns_indent">
+               <div class="sc_columns_item"><img alt="" src="images/assets/story.png"></div>
+                  <div class="sc_columns_item">
+                     <h5><a>3-Generational Tradition</a></h5>
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              Baia and her younger siblings — Gvantsa (Sister) and Giorgi (Brother) were born and raised in Obcha in a friendly, traditional
+                              family of winemakers. They spent a happy childhood watching how parents and grandparents were involved in the diverse
+                              winemaking processes. Children were also allowed to arrange their own small farm with vineyards, greens, household animals
+                              and plantations, and spent their free time exploring the wonders of nature. At that time, the Abuladze family traditionally
+                              produced wine using Imeretian technology — the maceration process (soaking the juice of the grape with the skins and
+                              sometimes also the stems) was shorter and only 5-10% of grape skin was used during fermentation in Qvevri.
+                              </div>
+                              </br>
+                              <div class="col-md-12 raleway small-font">
+                              After graduating from college, Baia moved to the capital city of Tbilisi to continue her studies, however she did not find
+                              the intense urban life interesting and so after agronomical university she returned to her small town of Obcha where she
+                              has been actively promoting the wines of Imereti and growing her family winery.
+                              </div>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                 
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="sc_content mainWrap" style="width: 100%;background: #EBEBEA;">
+         <div class="sc_event" style="padding: 6%;">
+            <div class="sc_event_item">
+               <div class="sc_columns_1 sc_columns_indent">
+                  <div class="sc_columns_item">
+                     <h5><a>The New Life of the Winery</a></h5>
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              In 2015, the Abuladze’s family business participated in the local agricultural startup competition and won the grant of
+                              GEL 5000 (about $2,000 USD at the time) to buy a used wine bottling machine to increase production and quality. At the
+                              same time, Baia officially established the new wine company and the label Baia’s Wine was born. Accompanied by her younger
+                              siblings, she totally remodelled the production structure and focused on the organic winemaking techniques. The family’s
+                              attitude towards each step of winemaking changed: the maceration process increased up to 3 months, each Qvevri was treated
+                              individually — more time was aligned for degustation to decide whether the liquid is ready for the further treatment; and the
+                              decision about what percent of the grape skin is used during fermentation and aging was given priority.
+                              </div>
+                              </br>
+                              <div class="col-md-12 raleway small-font">
+                              Next came a new chapter for wines from Imereti. They studied each of their varieties and their terroir. Their first organic
+                              wine — a white dry Tsolikouri was bottled in 2015. Soon afterwards, the women-empowered family business became
+                              one of the first organic wine producers in Bagdati Municipality.
+                              </div>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                 
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="sc_content mainWrap">
+         <div class="sc_event">
+            <div class="sc_event_item">
+               <div class="sc_columns_2 sc_columns_indent">
+              
+                  <div class="sc_columns_item">
+                     <h5><a>3-Generational Tradition</a></h5>
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              Baia and her younger siblings — Gvantsa (Sister) and Giorgi (Brother) were born and raised in Obcha in a friendly, traditional
+                              family of winemakers. They spent a happy childhood watching how parents and grandparents were involved in the diverse
+                              winemaking processes. Children were also allowed to arrange their own small farm with vineyards, greens, household animals
+                              and plantations, and spent their free time exploring the wonders of nature. At that time, the Abuladze family traditionally
+                              produced wine using Imeretian technology — the maceration process (soaking the juice of the grape with the skins and
+                              sometimes also the stems) was shorter and only 5-10% of grape skin was used during fermentation in Qvevri.
+                              </div>
+                              </br>
+                              <div class="col-md-12 raleway small-font">
+                              After graduating from college, Baia moved to the capital city of Tbilisi to continue her studies, however she did not find
+                              the intense urban life interesting and so after agronomical university she returned to her small town of Obcha where she
+                              has been actively promoting the wines of Imereti and growing her family winery.
+                              </div>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="sc_columns_item"><img alt="" src="images/assets/baias-winery.png"></div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="sc_content mainWrap">
+         <div class="sc_event">
+            <div class="sc_event_item">
+               <div class="sc_columns_2 sc_columns_indent">
+               <div class="sc_columns_item"><img alt="" src="images/assets/baias-winery.png"></div>
+                  <div class="sc_columns_item">
+                     <h5><a>Constant Development</a></h5>
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              Despite the family’s winemaking experience, the first
+                              steps for Baia and her siblings were not easy: Baia’s
+                              passion was to create a sustainable vineyard and to
+                              learn everything possible about a wine making process
+                              that reflected nature’s own process. Within that mission,
+                              her younger sister Gvantsa travelled to Mosel, Germany,
+                              as an intern to one of the best biodynamic wineries to
+                              obtain knowledge and a deep dive into the magic
+                              process of making organic wine
+                              </div>
+                              </br>
+                              <div class="col-md-12 raleway small-font">
+                              The grant that Baia won allowed for the family to bottle
+                              a higher standard wine, but she could not afford to buy
+                              a car to deliver those first bottles to Tbilisi. With her
+                              siblings’ help, she was able to carry them by train and
+                              deliver to each distribution channel by Tbilisi public
+                              transport. Amazingly, those bottles turned into a massive
+                              success: restaurants and wine bars ordered more bottles immediately and customers gave great feedback. Soon afterwards,
+                              the family business became part of the Georgian Farmers’ Association, which, in its turn, brought publicity in popular TV shows,
+                              as well as print media
+                              </div>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="sc_columns_1 sc_columns_indent">
+                  <div class="sc_columns_item">
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              Publicity and fame brought up another challenge: restaurants ordered more quantities while the young entrepreneurs found it
+                              quite hard to manage large-scale production from bottling to delivery in a timely manner. In parallel with running business,
+                              all of them had their university studies ongoing and parents were not familiar with Tbilisi streets to deliver the bottles
+                              to the partners’ establishments. The young trio accelerated their production by organizing equipment: they gathered and
+                              washed the old family pitchers that had been long forgotten and were able to harvest enough grapes for about 8,000 bottles.
+                              Business continued to grow. As the younger generation of the winemakers were more focused on innovative organic techniques,
+                              the siblings decided they needed to expand their knowledge again in the agricultural field. Giorgi decided to study enology and
+                              Gvantsa was enrolled in the master of agronomy at Georgian Agricultural University
+                              </div>
+                              </br>
+                              <div class="col-md-12 raleway small-font">
+                              A few years after starting with white wines, the trio offered red wine from Otskhanusi Sapere and Aladasturi grapes. Today,
+                              they produce wines from Imeretian vine varieties such as Tsolikouri, Tsitska, Krakhuna, Aladasturi, Otskhanuri Sapere.
+                              Each grape cluster is chosen carefully only from the Abuladze’s Obcha vineyards. The grape crushing, fermentation and aging
+                              is all done by the trio in the family cellar, to proudly offer the highest-quality organic Georgian wine to the world
+                              </div>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+            </div>
+         </div>
+      </div>
+      <div class="sc_content mainWrap" style="width: 100%;background: #EBEBEA;">
+         <div class="sc_event">
+            <div class="sc_event_item">
+               <div class="sc_columns_2 sc_columns_indent" style="padding:4%">
+                  <div class="sc_columns_item">
+                     <h5><a>About Obcha Village</a></h5>
+                     <div class="eventExcerpt">
+                        <div class="sc_columns  sc_columns_indent">
+                           <div class="sc_columns_item" style="width: 100%;">
+                              <div class="col-md-12 raleway small-font">
+                              The village Obcha boasts a unique microclimate for
+                              growing grapes. The location is unique — sitting in the
+                              eastern part of the Sairme Mountains which gives a slightly
+                              higher angle for the rays of the sun and a greater solar
+                              intensity. At 324 meters of altitude, the land offers an ideal
+                              location for growing premium wine grapes:
+                              The well-balanced geography, micro-climate and
+                              well-drained soil come together to create the perfect
+                              environment for Bais’s exquisitely handcrafted wines.
+                              The heavy cold air collected between the high peaks
+                              during the night drains off the heights, much like water,
+                              joining cold moist air, creating a double cooling effect.
+                              The cool nighttime temperatures are critical in developing
+                              high quality grapes. The alluvial soil, with clay, gravel,
+                              sand and limestone has another significant influence on
+                              wine quality. The limestone offers good drainage in the wet
+                              years, retaining much needed moisture in the dry vintages,
+                              while adding various mineral elements to the soils, which
+                              transfer to the gorgeous grapes.
+                              </div>
+                              </br>
+                              </br>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="sc_columns_item"><img alt="" src="images/assets/baias-winery.png"></div>
                </div>
             </div>
          </div>
