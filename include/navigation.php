dbc1ff43e2447195bbcdc54f8f6baed6ee884046
G-Apxo <giorgi.a@synvestment.co>
2021-04-20 18:48:22 +0400

    favicon + naigation fix

--- include/navigation.php
+++ include/navigation.php
@@ -7,26 +7,25 @@
          <div class="topMenu main">
             <ul id="mainmenu" class="sf-js-enabled">
                <li class="menu-item current-menu-ancestor current-menu-parent menu-item-has-children">
-                  <a href="#">Home</a>
+                  <a href="index.php">Home</a>
                   
                </li>
-               <li class="menu-item menu-item-has-children">
-                  <a href="#">Our Story</a>
+               <li class="menu-item ">
+                  <a href="our_story.php">Our Story</a>
                   
                </li>
-               <li class="menu-item menu-item-has-children">
+               <li class="menu-item">
                   <a href="#">Cardano Blockchain</a>
                   
                </li>
               
             </ul>
             <ul id="mainmenu_right" class="sf-js-enabled">
-            <li class="menu-item menu-item-has-children">
+            <li class="menu-item ">
                   <a href="our_wine.php">Our Wines</a>
-                
                </li>
                <li class="menu-item"><a href="shop.php">Media</a></li>
-               <li class="menu-item menu-item-has-children">
+               <li class="menu-item ">
                   <a href="#">Gallery</a>
                </li>
                <li class="menu-item"><a href="contacts.php">Contacts</a></li>
