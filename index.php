f117fdab948e6a915358d18642c993a5ecdef1c0
G-Apxo <39370807+GiorgiApkhadze@users.noreply.github.com>
2022-04-07 21:37:39 +0400

    new update for baiaswine adding media files

--- index.php
+++ index.php
@@ -41,7 +41,6 @@
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price" data-x="170" data-y="540" data-speed="300" data-start="2100" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <div class="tp-layer-inner-rotation black  rs-slideloop" data-easing="Power3.easeInOut" data-speed="2" data-xs="0" data-xe="0" data-ys="0" data-ye="0">
-                       
                      </div>
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 name" data-x="350" data-y="500" data-speed="300" data-start="2100" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
@@ -51,7 +50,6 @@
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price" data-x="350" data-y="540" data-speed="300" data-start="2200" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <div class="tp-layer-inner-rotation black  rs-slideloop" data-easing="Power3.easeInOut" data-speed="2" data-xs="0" data-xe="0" data-ys="0" data-ye="0">
-                        
                      </div>
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 name" data-x="550" data-y="500" data-speed="300" data-start="2200" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
@@ -61,7 +59,6 @@
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price" data-x="550" data-y="545" data-speed="300" data-start="2300" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <div class="tp-layer-inner-rotation black  rs-slideloop" data-easing="Power3.easeInOut" data-speed="2" data-xs="0" data-xe="0" data-ys="0" data-ye="0">
-                        
                      </div>
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 name" data-x="750" data-y="500" data-speed="300" data-start="2300" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
@@ -71,7 +68,6 @@
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price" data-x="750" data-y="545" data-speed="300" data-start="2400" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <div class="tp-layer-inner-rotation black  rs-slideloop" data-easing="Power3.easeInOut" data-speed="2" data-xs="0" data-xe="0" data-ys="0" data-ye="0">
-                        
                      </div>
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 name" data-x="950" data-y="505" data-speed="300" data-start="2400" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
@@ -81,17 +77,14 @@
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price" data-x="950" data-y="545" data-speed="300" data-start="2500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <div class="tp-layer-inner-rotation black  rs-slideloop" data-easing="Power3.easeInOut" data-speed="2" data-xs="0" data-xe="0" data-ys="0" data-ye="0">
-                        
                      </div>
                   </div>
                </li>
                <li data-transition="fade" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
                   <img src="images/transparent.png" alt="" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
                   <div class="tp-caption black tp-fade fadeout tp-resizeme rs-parallaxlevel-0 description" data-x="646" data-y="306" data-speed="300" data-start="500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
-                  
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 price-2" data-x="645" data-y="425" data-speed="800" data-start="500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="800">
-                     
                   </div>
                   <div class="tp-caption sfl stl rs-parallaxlevel-0" data-x="-26" data-y="45" data-speed="300" data-start="500" data-easing="Power3.easeInOut" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300">
                      <img src="images/korz.png" alt="">
@@ -106,10 +99,11 @@
                      <img src="images/vn.png" alt="">
                   </div>
                   <div class="tp-caption sfr str rs-parallaxlevel-0" data-x="635" data-y="91" data-speed="800" data-start="500" data-easing="Power3.easeInOut" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="800">
-				  <h1 class="">Welcome</h1></br><h3>To Baia's Wine!</h3>
+                     <h1 class="">Welcome</h1>
+                     </br>
+                     <h3>To Baia's Wine!</h3>
                   </div>
                   <div class="tp-caption black sfb stb tp-resizeme rs-parallaxlevel-0 button" data-x="647" data-y="494" data-speed="1200" data-start="500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="1200">
-                     
                   </div>
                </li>
             </ul>
@@ -129,7 +123,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Favourite Place</h4>
                            <div class="isotopeExcerpt">Lovely relaxed atmosphere of the winery gives you a positive...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -144,7 +138,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">In Vino Veritas</h4>
                            <div class="isotopeExcerpt">There’s no better feeling than uncorking a bottle of Chardonnay at...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -159,7 +153,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Wine Essentials</h4>
                            <div class="isotopeExcerpt">We make wine with a thought of our customers, therefore the team...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -174,7 +168,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Winemaking Is An Art</h4>
                            <div class="isotopeExcerpt">There’s truth in wine, and there may be some in gin and muddy...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -189,7 +183,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Organic Grapes</h4>
                            <div class="isotopeExcerpt">Our wine is a combination of hard work of our wine makers and the...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -204,7 +198,7 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Sustainable Farmed</h4>
                            <div class="isotopeExcerpt">We adhere to the winemaking principles for you to be our returning...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
@@ -219,16 +213,14 @@
                         <!-- <div class="isotopeContent">
                            <h4 class="isotopeTitle">Fabulous Taste</h4>
                            <div class="isotopeExcerpt">Wine House does its best to ensure that each of their clients gets...</div>
-                        </div> -->
+                           </div> -->
                      </div>
                   </div>
                </div>
             </article>
          </section>
       </div>
-      
       <div id="cardano" class="sc_section sc_align_center blocks_5" style="background-image: url(images/wine-two.PNG);background-repeat: no-repeat;background-size: contain;"  >
-      
          <div class="sc_content mainWrap">
             <div class="sc_event">
                <div class="sc_event_item">
@@ -239,13 +231,13 @@
                            Baia’s Wine in collaboration with <a href="https://www.cardanofoundation.org/">Cardano Foundation</a> is preserving the 8,000 years history of the Georgian wine industry. Through the use of the Cardano blockchain, we are bringing unparalleled transparency to our supply chain to authenticate Baia’s Wine from farm to table.
                            Baia’s Wine supply chain is managed by the unique Cardano Blockchain system, enabling consumers to easily verify the wines authenticity: A full record of the supply chain data is stored on the Cardano blockchain, enabling to clearly track and record the supply chain activities for Baia’s wine from farm to table.
                            The Cardano Foundation is an independent Swiss not-for-profit organization that oversees and supervises the advancement of Cardano and its ecosystem. The Foundation’s core focus is in developing a Cardano adoption strategy through integration and execution, to enable fast-track value creation for inclusive and equitable growth. 
-						         As the custodian of the protocol and the owner of the Cardano brand, the Foundation works to drive adoption and partnerships, grow the wider blockchain community, shape legislation, and commercial standards, and ensure stakeholder accountability.
-						   </br>
+                           As the custodian of the protocol and the owner of the Cardano brand, the Foundation works to drive adoption and partnerships, grow the wider blockchain community, shape legislation, and commercial standards, and ensure stakeholder accountability.
+                           </br>
                         </div>
                         <div class="eventMore"><a href="https://forum.cardano.org/t/cardano-foundation-develops-metadata-connector-for-scantrust-supply-chain-tracking/42741" class="sc_button sc_button_skin_dark sc_button_style_bg sc_button_size_big">Find out more</a></div>
                      </div>
                      <div class="sc_columns_item" style="align-items: flex-end;justify-content: flex-end;display: flex;">
-                     <img alt="" src="images/cardano.png"style="width:50%">
+                        <img alt="" src="images/cardano.png"style="width:50%">
                      </div>
                   </div>
                </div>
@@ -255,10 +247,9 @@
       <div class="sc_section sc_align_center" id="meida">
          <div class="sc_content mainWrap custom_mar_3">
             <h4 class="sc_title sc_title_center custom_title_1" style="padding-bottom: 5%;margin-top: -4%;">MEDIA</h4>
-            
             <div class="sc_blogger sc_blogger_horizontal style_image style_image_large sc_blogger_indent">
                <div class="sc_columns_6 sc_columns_indent">
-                  <div class="sc_columns_item" style="margin-left: 7%;">
+                  <div class="sc_columns_item" style="margin-left: 3%;">
                      <article class="sc_blogger_item">
                         <div class="thumb">
                            <a href="#"><img alt="" src="images/assets/0828_forbes-logo_650x455.jpg" style="width: 76%;margin-left: 14%;"></a>
@@ -266,49 +257,60 @@
                         <h6 class="sc_blogger_title sc_title"><a >Meet The 25-Year-Old Georgian Winemaker Named To Forbes’ 2019 30 Under 30 Europe List</a></h6>
                         </br>
                         <div class="copy-paste-block" style="text-align: center;">
-                       <a href="https://www.forbes.com/sites/chloesorvino/2019/02/12/meet-the-25-year-old-georgian-winemaker-named-to-forbes-2019-30-under-30-europe-list/?sh=7fc2fbdf3f53">Read more</a> 
-								</div>
+                           <a href="https://www.forbes.com/sites/chloesorvino/2019/02/12/meet-the-25-year-old-georgian-winemaker-named-to-forbes-2019-30-under-30-europe-list/?sh=7fc2fbdf3f53">Read more</a> 
+                        </div>
                         <div class="sc_blogger_content">
-                           
                         </div>
                      </article>
                   </div>
-                  <div class="sc_columns_item"style="margin-left: 7%;" >
+                  <div class="sc_columns_item"style="margin-left: 3%;" >
                      <article class="sc_blogger_item">
                         <div class="thumb">
                            <a href="#"><img alt="" src="images/assets/amber-champion.png"></a>
                         </div>
                         <h6 class="sc_blogger_title sc_title"><a >Simon J Woolf explaining the traditions behind the technique and recommending 30 of his favourite skin-contact discoveries</a></h6>
                         <div class="sc_blogger_content">
-                        <div class="copy-paste-block" style="text-align: center;">
-                       <a href="images/Top-30-orange-wines.pdf">Read more</a> 
-								</div>
+                           <div class="copy-paste-block" style="text-align: center;">
+                              <a href="images/Top-30-orange-wines.pdf">Read more</a> 
+                           </div>
                         </div>
                      </article>
                   </div>
-                  <div class="sc_columns_item" style="margin-left: 7%;">
+                  <div class="sc_columns_item" style="margin-left: 3%;">
                      <article class="sc_blogger_item">
                         <div class="thumb">
                            <a href="#"><img alt="" src="images/assets/1200px-The_Logo_of_The_Washington_Post_Newspaper.svg.png"></a>
                         </div>
                         <h5 class="sc_blogger_title sc_title"><a href="#">5 wines to try when you’re in the mood to pour something different</a></h5>
                         <div class="sc_blogger_content">
-                        <div class="copy-paste-block" style="text-align: center;">
-                       <a href="https://www.washingtonpost.com/lifestyle/food/5-wines-to-try-when-youre-in-the-mood-to-pour-something-different/2018/10/05/a63897a4-c805-11e8-b1ed-1d2d65b86d0c_story.html?arc404=true">Read more</a> 
-								</div>
+                           <div class="copy-paste-block" style="text-align: center;">
+                              <a href="https://www.washingtonpost.com/lifestyle/food/5-wines-to-try-when-youre-in-the-mood-to-pour-something-different/2018/10/05/a63897a4-c805-11e8-b1ed-1d2d65b86d0c_story.html?arc404=true">Read more</a> 
+                           </div>
                         </div>
                      </article>
                   </div>
-                  <div class="sc_columns_item" style="margin-left: 7%;">
+                  <div class="sc_columns_item" style="margin-left: 3%;">
                      <article class="sc_blogger_item">
                         <div class="thumb">
                            <a href="#"><img alt="" src="images/assets/food-and-wine-logo-vector.png"></a>
                         </div>
                         <h5 class="sc_blogger_title sc_title"><a href="#"> This 25-Year-Old Winemaker Is Making Some of Georgia’s Buzziest Wines </a></h5>
                         <div class="sc_blogger_content">
-                        <div class="copy-paste-block" style="text-align: center;">
-                       <a href="https://www.foodandwine.com/wine/baia-abuladze-georgian-wine">Read more</a> 
-								</div>
+                           <div class="copy-paste-block" style="text-align: center;">
+                              <a href="https://www.foodandwine.com/wine/baia-abuladze-georgian-wine">Read more</a> 
+                           </div>
+                        </div>
+                     </article>
+                  </div>
+                  <div class="sc_columns_item" style="margin-left: 3%;">
+                     <article class="sc_blogger_item">
+                        <div class="thumb">
+                           <a href="https://www.travelmyth.com/Obcha/Hotels/dog_friendly"><img src="https://photos.travelmyth.com/awards/travelmyth_1600202__dog_friendly_p0_yen_web.png" width="160" border="0" alt="Obcha dog friendly hotels" /></a>
+                        </div>
+                        <div class="sc_blogger_content">
+                           <div class="copy-paste-block" style="text-align: center;">
+                              <a href="https://www.foodandwine.com/wine/baia-abuladze-georgian-wine">Read more</a> 
+                           </div>
                         </div>
                      </article>
                   </div>
@@ -316,7 +318,6 @@
             </div>
          </div>
       </div>
-
    </div>
    <?php include 'include/footer.php';?>
 </body>
